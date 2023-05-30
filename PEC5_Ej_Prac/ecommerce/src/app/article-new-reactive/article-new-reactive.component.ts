101df5d26c5e869c14eebcf0a971cc6e7eb9995f
nacaru-w <ignaciocr92@gmail.com>
2023-05-31 00:29:35 +0200

    Copying template based form so as to adapt it

--- PEC5_Ej_Prac/ecommerce/src/app/article-new-reactive/article-new-reactive.component.ts
+++ PEC5_Ej_Prac/ecommerce/src/app/article-new-reactive/article-new-reactive.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ComponentCommunicationService } from '../component-communication.service';
+
 
 @Component({
   selector: 'app-article-new-reactive',
   templateUrl: './article-new-reactive.component.html',
   styleUrls: ['./article-new-reactive.component.scss']
 })
-export class ArticleNewReactiveComponent {
+
+export class ArticleNewReactiveComponent implements OnInit {
+
+  public hideForm: boolean = true;
+
+  constructor(private componentCommunicationService: ComponentCommunicationService) { }
+
+  ngOnInit(): void {
+    this.componentCommunicationService.hideReactiveFormEvent.subscribe(() => {
+      this.hideForm = true;
+    }),
+      this.componentCommunicationService.showReactiveFormEvent.subscribe(() => {
+        this.hideForm = false;
+      })
+  }
 
 }
